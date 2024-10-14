734aba8bc74abc1bf9ae2a290f37bce4b46d278b
MD NASIM <nasimubd@gmail.com>
2024-10-14 21:25:02 +0600

    Initial project setup

---
+++ epatner-pos/epatner-pos-backend/src/main/java/com/epatner/pos/EpatnerPosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.epatner.pos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpatnerPosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpatnerPosBackendApplication.class, args);
+	}
+
+}
