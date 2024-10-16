fef522a40454783d5b14bb95d81efbaf64b88111
MD NASIM <nasimubd@gmail.com>
2024-10-16 10:54:00 +0600

    Duplicate dependencies Removed

--- epatner-pos/epatner-pos-backend/src/main/java/com/userManagement/UserEntity.java
+++ epatner-pos/epatner-pos-backend/src/main/java/com/userManagement/UserEntity.java
@@ -1,4 +1,48 @@
 package com.userManagement;
 
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+// Lombok annotation to automatically generate getters, setters, equals, hashCode, and toString methods
+@Data
+// Marks this class as a JPA entity
+@Entity
+// Specifies the table name in the database
+@Table(name = "users")
 public class UserEntity {
+    // Marks this field as the primary key
+    @Id
+    // Configures the way of increment of the specified column(field)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // Specifies that the username should be unique and not null
+    @Column(unique = true, nullable = false)
+    private String bussinessName;
+
+    // Specifies that the password should not be null
+    @Column(nullable = false)
+    private String password;
+
+    // Specifies that the email should not be null
+    @Column(nullable = false)
+    private String email;
+
+    // Specifies the column name in the database
+    @Column(name = "first_name")
+    private String firstName;
+
+    // Specifies the column name in the database
+    @Column(name = "last_name")
+    private String lastName;
+
+    // Specifies that this field should be stored as a string in the database
+    @Enumerated(EnumType.STRING)
+    private Role role;
 }
+// Enum to represent user roles
+enum Role {
+    ADMIN, CASHIER, MANAGER
+}
+
--- epatner-pos/epatner-pos-backend/src/main/java/com/userManagement/UserRepository.java
+++ epatner-pos/epatner-pos-backend/src/main/java/com/userManagement/UserRepository.java
@@ -1,4 +1,9 @@
 package com.userManagement;
 
+//import com.epatner.pos.userManagement.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 public class UserRepository {
+
 }
